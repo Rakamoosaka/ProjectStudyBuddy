2e93531cb530bbd55a122d275c48b1b7c855081c
Aitore <aitore.master@gmail.com>
2024-12-04 04:25:26 +0500

    pls deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LogoImage from "../assets/logo.png";
+import LogoImage from "../assets/Logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
