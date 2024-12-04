ccb2a92dc6e6d87140380962cfa2597e60cbe015
Aitore <aitore.master@gmail.com>
2024-12-04 15:00:01 +0500

    trying to resolve the issue of the main banner not showing in vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    assetsDir: "assets", // Ensures assets are placed in the correct folder
+  },
 });
