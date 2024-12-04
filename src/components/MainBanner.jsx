4144a73f5972add0af8086d6d82f95718f728e0f
Aitore <aitore.master@gmail.com>
2024-12-04 15:04:57 +0500

    trying to resolve the issue of the main banner not showing in vercel second attempt

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import MainBoardSVG from '../assets/svg/MainBoardSVG';
-import svgsvg from '../assets/svg/svgsvg.svg';
+import React from "react";
+import MainBoardSVG from "../assets/svg/MainBoardSVG";
 
 const MainBanner = () => {
   return (
     <div
       className="w-11/12 sm:w-10/12 md:w-8/12 h-60 sm:h-80 md:h-96 lg:h-[30rem] flex justify-center items-center mt-8 sm:mt-12 md:mt-14 mx-auto relative"
       style={{
-        backgroundImage: `url(${svgsvg})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain',
-        backgroundPosition: 'center',
+        backgroundImage: `url(/svgsvg.svg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
       }}
     >
       <div className="flex flex-col items-center relative">
@@ -19,7 +18,7 @@ const MainBanner = () => {
         </div>
         <h1
           className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-[#162751] font-instrumentSans"
-          style={{ textShadow: 'black 1px 1px 3px' }}
+          style={{ textShadow: "black 1px 1px 3px" }}
         >
           STUDY BUDDY
         </h1>
@@ -32,4 +31,3 @@ const MainBanner = () => {
 };
 
 export default MainBanner;
-
